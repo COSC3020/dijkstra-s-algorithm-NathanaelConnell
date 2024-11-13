e787d5622a95844de4d32796d7de328a52b0a3f0
Nathanael Connell <nathanael@Nathanaels-MacBook-Pro.local>
2024-11-13 15:58:47 -0700

    Test Commit

--- code.js
+++ code.js
@@ -1,3 +1,33 @@
 function dijkstra(graph, sourceNode) {
-    return [];
+    
+    for (let i = 0; i < graph.length; i++) {
+        graph[i].distance = Infinity;
+        graph[i].visited = false;
+    }
+
+    graph[sourceNode].distance = 0;
+    
+    while (true) {
+        let currentNode = null;
+        let currentDistance = Infinity;
+        for (let i = 0; i < graph.length; i++) {
+            if (!graph[i].visited && graph[i].distance < currentDistance) {
+                currentNode = graph[i];
+                currentDistance = graph[i].distance;
+            }
+        }
+        if (currentNode === null) {
+            break;
+        }
+        for (let i = 0; i < currentNode.edges.length; i++) {
+            const neighbor = currentNode.edges[i];
+            const newDistance = currentNode.distance + neighbor.distance;
+            if (newDistance < graph[neighbor.node].distance) {
+                graph[neighbor.node].distance = newDistance;
+            }
+        }
+        currentNode.visited = true;
+    }
+
+    return graph.map(node => node.distance);
 }
